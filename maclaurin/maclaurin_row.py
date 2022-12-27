ba1279be407c43745f1f71870bbb9e729ef4e2af
p1petto <108504552+p1petto@users.noreply.github.com>
2022-12-28 02:15:06 +0800

    approximation

--- maclaurin/maclaurin_row.py
+++ maclaurin/maclaurin_row.py
@@ -1,4 +1,3 @@
-import math
 from add_func import *
 
 
@@ -7,6 +6,7 @@ def get_data_x(x_range, n, func):
     for x in range(x_range[0], x_range[1]):
         data_x.append(func(x, n))
     return data_x
+
 
 def get_maclaurin_e(x, n):
     e = 0
@@ -24,29 +24,29 @@ def get_maclaurin_sin(x, n):
         if i == 0:
             result += x
         else:
-            result += (-1 ** i) * (x ** (2*i+1)) / factorial(2*i+1)
+            result += (-1 ** i) * (x ** (2 * i + 1)) / factorial(2 * i + 1)
     return result
 
 
 def get_maclaurin_cos(x, n):
     result = 0
-    for i in range(n, -1, -1):
+    for i in range(n + 1):
         if i == 0:
             result += 1
         else:
-            result += (-1 ** i) * (x ** (2*i)) / factorial(2*i)
+            result += (-1 ** i) * (x ** (2 * i)) / factorial(2 * i)
     return result
 
 
 def get_maclaurin_arcsin(x, n):
     result = 0
-    for i in range(n, -1, -1):
+    for i in range(n + 1):
         if i == 0:
             result += x
         else:
-            result += (factorial(2*i) * (x ** (2*i+1))) / ((4 ** i) * (factorial(i) ** 2) * factorial(2*i+1))
+            result += (factorial(2 * i) * (x ** (2 * i + 1))) / ((4 ** i) * (factorial(i) ** 2) * factorial(2 * i + 1))
     return result
 
 
 def get_maclaurin_arccos(x, n):
-    return math.pi/2 - get_maclaurin_arcsin(x, n)
+    return math.pi / 2 - get_maclaurin_arcsin(x, n)
